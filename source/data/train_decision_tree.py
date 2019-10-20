4c82b3f6a28ee40ad603f9dc31f5bdd2d07e324d
Alex Pan <alexanderypan@gmail.com>
2019-10-20 18:08:48 -0400

    --

--- source/data/train_decision_tree.py
+++ source/data/train_decision_tree.py
@@ -10,13 +10,18 @@ import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 rps_data = pd.read_csv('data/intermediate/rps_data_12_past_moves.csv')
-features = ['minus1', 'minus2', 'minus3', 'minus4', 'minus5', 'minus6',
-            'minus7', 'minus8', 'minus10', 'minus11', 'minus12']
+# features = ['minus1', 'minus2', 'minus3', 'minus4', 'minus5', 'minus6',
+#             'minus7', 'minus8', 'minus10', 'minus11', 'minus12',
+#             'won_minus1', 'won_minus2', 'won_minus3', 'won_minus4', 'won_minus5',
+#             'won_minus6', 'won_minus7', 'won_minus8', 'won_minus9', 'won_minus10',
+#             'won_minus11', 'won_minus12']
+
+features = ['minus1', 'minus2', 'minus3', 'won_minus1', 'won_minus2', 'won_minus3']
 
 # Some minor recoding of variables
 throw, labels = pd.factorize(rps_data['throw'])  # xgboost requires classes start w/ 0
 
-rps_dummies = pd.get_dummies(rps_data[features], prefix=features)
+rps_dummies = pd.get_dummies(rps_data[features])
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(
         rps_dummies, throw, test_size=0.2, random_state=1)
@@ -28,15 +33,26 @@ x_train, x_test, y_train, y_test = model_selection.train_test_split(
 dtrain = xgb.DMatrix(x_train, label=y_train)
 dtest = xgb.DMatrix(x_test, label=y_test)
 
-param = {'max.depth': 2,'objective': 'multi:softmax', 'num_class': 3}
-bst = xgb.train(param, dtrain, num_boost_round=10)
+param = {'max.depth': 2,'objective': 'multi:softmax', 'num_class': 3, 'verbosity': 2}
+model = xgb.XGBClassifier(kwargs = param)
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_train)
+pd.Series(y_pred == y_train).value_counts()
+(y_pred == y_train).mean()
+
+y_test_pred = model.predict(x_test)
+pd.Series(y_test_pred == y_test).value_counts()
+(y_test_pred == y_test).mean()
+
 
 # bst.dump_model('test.txt')
 
-fig, ax = plt.subplots(figsize=(100, 100))
-gr = xgb.plot_tree(bst, num_trees=0, ax=ax, rankdir='LR')
-plt.savefig('test.pdf')
-plt.show()
+
+# fig, ax = plt.subplots(figsize=(100, 100))
+# gr = xgb.plot_tree(bst, num_trees=0, ax=ax, rankdir='LR')
+# plt.savefig('test.pdf')
+# plt.show()
 # preds = clf.predict(y_test)
 
 bst.get_score(importance_type='gain')
