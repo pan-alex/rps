fcdb48f6d88cc23107cbc8449752ef610a2e1231
Alex Pan <alexanderypan@gmail.com>
2019-12-13 22:13:22 -0500

    Refactor  to use a dictionary instead of nested if else statements. Should be easier to read and also will not break if the values used to represent R, P, S (i.e., ) are changed

--- source/game/rps_game.py
+++ source/game/rps_game.py
@@ -2,16 +2,11 @@ class InvalidInput_RPS(Exception): pass
 
 # Possible game inputs
 OPTIONS = [1, 2, 3]
-INPUTS_TO_STRINGS = {1:'R', 2:'P', 3:'S'}
-
-def game_message(outcome):
-    if outcome == 0: message = "It's a draw."
-    elif outcome == -1: message = "You lose :("
-    elif outcome == 1: message = "You win!"
-    return message
+INPUTS_TO_STRINGS = {1:'R', 2:'P', 3:'S'}    # Not sure if I'll even use these dicts
+STRINGS_TO_INPUTS = {string: input for input, string in INPUTS_TO_STRINGS.items()}    # Reverse the dict above
 
 
-def rps_round(input1, input2, print_message=False):
+def rps_round(input1, input2):
     '''
     # Evaluates one round of RPS. This is the lowest level function for the game.
     :param input1: This is the player input. String. 1, 2, or 3.
@@ -19,22 +14,20 @@ def rps_round(input1, input2, print_message=False):
     :return:
     '''
     global OPTIONS
-
     if (input1 not in OPTIONS) or (input2 not in OPTIONS):
         raise InvalidInput_RPS('Input must be 1 (Rock), 2 (Paper), or 3 (Scissors).')
 
-    if input1 == 1:
-        if input2 == 1: outcome = 0
-        elif input2 == 2: outcome = -1
-        elif input2 == 3: outcome = 1
-    elif input1 == 2:
-        if input2 == 2: outcome = 0
-        elif input2 == 3: outcome = -1
-        elif input2 == 1: outcome = 1
-    elif input1 == 3:
-        if input2 == 3: outcome = 0
-        elif input2 == 1: outcome = -1
-        elif input2 == 2: outcome = 1
+    r, p, s = OPTIONS
+    win_matrix = {r: {r: 0,  # i.e., if input1 is rock, input2 is rock = tie
+                      p: -1,  # input2 is paper = loss
+                      s: 1},  # input2 is scissors = win
+                  p: {r: 1,
+                      p: 0,
+                      s: -1},
+                  s: {r: -1,
+                      p: 1,
+                      s: 0}
+                  }
 
-    if print_message == True: print(game_message(outcome))
+    outcome = win_matrix[input1][input2]
     return outcome
