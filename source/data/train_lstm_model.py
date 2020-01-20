8769899a6f847e053136e4b86dbe6568f6335b1a
Alex Pan <alexanderypan@gmail.com>
2020-01-19 20:11:25 -0500

    First working version of LSTM (fixed data structure and RNN architecture). Still need to fine-tune architecture and assess model

--- source/data/train_lstm_model.py
+++ source/data/train_lstm_model.py
@@ -1,54 +1,90 @@
 import numpy as np
-import math
 import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Activation
-
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential, model_from_json
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
 
 ###
-#### Read in data
+#### Prepare Data
 ###
+def one_hot_encode(input, nb_classes=3):
+    '''
+    Based off https://stackoverflow.com/questions/38592324/one-hot-encoding-using-numpy#38592416
+    '''
+    input = input.astype(int)   # Must be an integer to use as index
+    # RPS inputs are R=1, P=2, S=3, and missing = 0. Therefore shift all inputs
+    # down by 1 so that R = 0, P = 1, S = 2, missing = -1 (i.e., last row)
+    input_to_index = input.astype(int) - 1
+    # 3 is the # of possible inputs; 4 rows (with 1 row of all 0s for missing values)
+    one_hot_encoded_array = np.eye(N=nb_classes+1, M=nb_classes)[input_to_index]
+    return one_hot_encoded_array
 
-# Exclude the first line, which contains headings
-rps_as_array = np.genfromtxt('data/intermediate/rps_data_sequence.csv', delimiter=',')[1:]
 
-# Reverse the order of the array, so that the last element is the most recent throw
-rps_as_array_rev = np.flip(rps_as_array, axis=1)
+rps = pd.read_csv('data/intermediate/rps_data_sequence.csv')
+rps = rps.fillna(0)    # Fill NaNs with 0.
 
-# Remove nans; first we have to transform the data to a list, then filter nans
-rps_as_array_rev_list = np.ndarray.tolist(rps_as_array_rev)
+# Extract the throw column, which will be the ground-truth labels for the prediction
+Y = one_hot_encode(np.array(rps.throw))
 
-# Not clean and probably very inefficient but does the job...
-n = len(rps_as_array_rev_list)
-rps_clean =  [[] for _ in range(n)]
-for i in range(n):
-    rps_clean[i] = []
-    for element in rps_as_array_rev_list[i]:
-        if math.isnan(element) == False:
-            rps_clean[i].append(element)
+# Extract the remaining columns.
+# Reverse the order of the array so that the last element is the most recent throw.
+rps_X = rps.drop(['throw'], axis=1)
+rps_X = rps_X[rps_X.columns[::-1]]
 
-X = rps_clean
+# Convert the remaining columns to one-hot-encoded arrays. Sequences are zero-padded for missing values.
+X = one_hot_encode(rps_X)
+
+### Some tests to compare that the results are correct
+# rps_X.iloc[201]
+# X[201]
+# rps_X.iloc[20001]
+# X[20001]
+###
 
 # Generate data for keras
-
-
-
-
-###
-#### Build Vocabulary
-###
-vocab = [1, 2, 3]
-vocab_size = len(vocab)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
 
 
 ###
 #### Build model
 ###
+batch_size = None    # unspecified training mini-batch size; let Keras handle
+T_x = 12    # Number of time steps to include
+n_x = 3    # Length of x at each time step (i.e., vocab size)
 
-#
+# Define model architecture
 model = Sequential()
-model.add(LSTM(4, return_sequences=True))
-model.add(Activation('softmax'))
+model.add(LSTM(units=1, batch_input_shape=(None, T_x, n_x), return_sequences=False))
+model.add(Dense(3, activation='softmax'))
 
+# Initialize model (note model can be trained continuously as long as it is not re-initialized)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
+# Train the model
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test), shuffle=False, verbose=2)
+
+
+###
+#### Assess model
+###
+# results = model.predict(x_test)
+# plt.scatter(range(100), y_test[:100][0], c='r')
+plt.plot(history.history['loss'])
+plt.show()
+
+
+###
+#### Save and load model
+###
+# # serialize model to JSON
+# model_json = model.to_json()
+# with open("source/data/lstm_model1.json", "w") as json_file:
+#     json_file.write(model_json)
+# model.save_weights("source/data/lstm_model1.h5")
+#
+# # load json and create model
+# with open('source/data/lstm_model1.json', 'r') as json_file:
+#     loaded_model_json = json_file.read()
+# loaded_model = model_from_json(loaded_model_json)
+# loaded_model.load_weights("source/data/model.h5")
